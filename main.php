7b42b24e59839493a4783a31c757fd644f336b17
Keiko Sato <k-sato@paseli.co.jp>
2022-12-14 20:56:02 +0900

    add bootstrap

--- main.php
+++ main.php
@@ -5,11 +5,34 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="css/style.css">
-  <title>Document</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <title>TODO</title>
 </head>
 <body>
 
 <h1>TODO</h1>
-  
+
+<div class="col-md-4">
+  <form>
+    <div class="mb-3">
+      <label for="date" class="form-label">日付</label>
+      <input type="date" class="form-control" id="date">
+    </div>
+    <div class="mb-3">
+      <label for="summary" class="form-label">サマリー</label>
+      <input type="text" class="form-control" id="summary">
+    </div>
+    <div class="mb-3">
+      <label for="summary" class="form-label">詳細</label>
+      <textarea  class="form-control" rows="3"></textarea>
+    </div>
+    <!-- <div class="mb-3 form-check">
+      <input type="checkbox" class="form-check-input" id="exampleCheck1">
+      <label class="form-check-label" for="exampleCheck1">Check me out</label>
+    </div> -->
+    <button type="submit" class="btn btn-primary">Submit</button>
+  </form>
+</div>
+
 </body>
 </html>
